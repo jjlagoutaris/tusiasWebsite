ea23a6a7fe2a9225b852d1cc48c2bd82595d7d75
John Lagoutaris <jjlagoutaris@gmail.com>
2023-01-21 23:38:42 -0500

    Set up files for containers, components, and pages for a SPA.

---
+++ frontend/src/containers/index.js
@@ -0,0 +1,5 @@
+export { default as About } from './About/About';
+export { default as Footer } from './Footer/Footer';
+export { default as Header } from './Header/Header';
+export { default as Blog } from './Blog/Blog';
+export { default as Services } from './Services/Services';
