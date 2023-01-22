e68f90e919e67ce7953104c4bdbd76bd5334cf30
John Lagoutaris <jjlagoutaris@gmail.com>
2023-01-21 23:38:42 -0500

    Set up files for containers, components, and pages for a SPA.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
-import './App.css';
+import { About, Footer, Header, Services, Blog } from './containers'
+import {About, Blog, Contact, Home, Services, NotFound} from './pages';
+import {Link, Route, Routes} from "react-router-dom"
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
+      <About />
     </div>
   );
 }
---
+++ frontend/src/pages/index.js
@@ -0,0 +1,6 @@
+export { default as About } from './About';
+export { default as Blog } from './Blog';
+export { default as Contact} from './Contact';
+export { default as Home } from './Home';
+export { default as NotFound } from './NotFound';
+export { default as Services } from './Services';
