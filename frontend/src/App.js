516801a642f948089fb9eafe2bb3e8f99039425b
John Lagoutaris <jjlagoutaris@gmail.com>
2023-01-22 00:47:11 -0500

    Link up SPA routes and add Navbar for generating the pages.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
 
-import { About, Footer, Header, Services, Blog } from './containers'
+// import { About, Footer, Header, Services, Blog } from './containers'
 import {About, Blog, Contact, Home, Services, NotFound} from './pages';
 import {Link, Route, Routes} from "react-router-dom"
 import './App.scss';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <About />
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/Home" element={<Home />} />
+        <Route path="/Blog" element={<Blog />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/Services" element={<Services />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/NotFound" element={<NotFound />} />
+      </Routes>
+    </>
   );
 }
 
