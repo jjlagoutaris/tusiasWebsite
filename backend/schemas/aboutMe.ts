8d48ef9bc97d42880eaf81c383ef22c6599374e8
John Lagoutaris <jjlagoutaris@gmail.com>
2023-02-24 17:37:40 -0500

    Update backend schemas to trim out unnecessary fields

---
+++ backend/schemas/aboutMe.ts
@@ -0,0 +1,27 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'aboutMe',
+  title: 'About Me',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'header',
+      title: 'Header',
+      type: 'string',
+    }),
+    defineField({
+      name: 'bio',
+      title: 'Bio',
+      type: 'string',
+    }),
+    defineField({
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    }),
+  ],
+})
