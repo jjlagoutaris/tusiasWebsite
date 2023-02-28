016ac216635715abde524a14d9443f0d3ff90313
John Lagoutaris <jjlagoutaris@gmail.com>
2023-02-28 04:58:10 -0500

    Implement Blog page and responsiveness

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,5 +1,6 @@
 export { default as AboutPage } from './AboutPage';
 export { default as BlogPage } from './BlogPage';
+export { default as BlogPost } from './BlogPost';
 export { default as ContactPage } from './ContactPage';
 export { default as Home } from './Home';
 export { default as NotFound } from './NotFound';
