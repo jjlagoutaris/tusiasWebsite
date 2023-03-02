df165b1e14005e059a1d46ec0b5716f76334cd02
John Lagoutaris <jjlagoutaris@gmail.com>
2023-03-02 04:12:33 -0500

    Set up for deployment to Netlify

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,8 +7,8 @@ import { HashRouter, BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
