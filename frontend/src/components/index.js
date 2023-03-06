4b351bc252ba065e49207fec4ff6a1d8d2c60ef7
John Lagoutaris <jjlagoutaris@gmail.com>
2023-03-06 02:53:51 -0500

    Add ImageComponent for blog posts

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -3,3 +3,4 @@ export { default as HelloComponent } from './HelloComponent/HelloComponent';
 export { default as BlogComponent } from './BlogComponent/BlogComponent';
 export { default as ServicesComponent } from './ServicesComponent/ServicesComponent';
 export { default as ScrollToTop } from './ScrollToTop/ScrollToTop';
+export { default as components } from './CustomImageComponent/CustomImageComponent';
